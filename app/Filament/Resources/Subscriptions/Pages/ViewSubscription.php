23b49e2651c7151e4b0825eb559300c4da3c674e
Rayan TSOLEFACK <68066376+rayanfalcao8@users.noreply.github.com>
2026-07-16 15:52:34 -0400

    test(admin): finalize operations v2 coverage

--- app/Filament/Resources/Subscriptions/Pages/ViewSubscription.php
+++ app/Filament/Resources/Subscriptions/Pages/ViewSubscription.php
@@ -2,14 +2,8 @@
 
 namespace App\Filament\Resources\Subscriptions\Pages;
 
-use App\Domain\Subscription\Actions\ManageSubscription;
 use App\Filament\Resources\Subscriptions\SubscriptionResource;
-use App\Models\Subscription;
-use Filament\Actions\Action;
-use Filament\Forms\Components\TextInput;
-use Filament\Notifications\Notification;
 use Filament\Resources\Pages\ViewRecord;
-use RuntimeException;
 
 class ViewSubscription extends ViewRecord
 {
