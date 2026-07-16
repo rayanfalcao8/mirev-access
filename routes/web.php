95edbf6ae54079dbe8527e121fcc6354d96c433d
Rayan TSOLEFACK <68066376+rayanfalcao8@users.noreply.github.com>
2026-07-16 12:22:50 -0400

    feat: expose client portal routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\ClientPortalController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', '/connect/demo');
+
+Route::get('/connect/{site:slug}', [ClientPortalController::class, 'show'])
+    ->name('client.portal');
+
+Route::post('/connect/{site:slug}/purchase', [ClientPortalController::class, 'purchase'])
+    ->name('client.purchase');
+
+Route::get('/connect/{site:slug}/subscriptions/{subscription}', [ClientPortalController::class, 'success'])
+    ->name('client.success');
