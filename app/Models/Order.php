7ccdc943236f3e5b6102fed9e07ce26c37bd9fd3
Rayan TSOLEFACK <68066376+rayanfalcao8@users.noreply.github.com>
2026-07-16 23:57:07 -0400

    refactor(payment): require confirmation before access

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Order extends Model
@@ -28,6 +29,11 @@ class Order extends Model
     public function subscription(): HasOne
     {
         return $this->hasOne(Subscription::class);
+    }
+
+    public function paymentAttempts(): HasMany
+    {
+        return $this->hasMany(PaymentAttempt::class);
     }
 
     public function plan(): BelongsTo
