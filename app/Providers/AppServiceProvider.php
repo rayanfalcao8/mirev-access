0d10f2aea9677a88ac4d7f52b83b9100af383d56
Rayan TSOLEFACK <68066376+rayanfalcao8@users.noreply.github.com>
2026-07-16 23:41:42 -0400

    feat(network): register site providers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Domain\Access\Contracts\NetworkAccessProvider;
+use App\Domain\Access\Services\NetworkProviderRegistry;
 use App\Infrastructure\Network\FakeAccessProvider;
 use Illuminate\Support\ServiceProvider;
 
@@ -10,7 +11,15 @@ class AppServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
+        $this->app->singleton(FakeAccessProvider::class);
         $this->app->bind(NetworkAccessProvider::class, FakeAccessProvider::class);
+
+        $this->app->singleton(
+            NetworkProviderRegistry::class,
+            fn ($app) => new NetworkProviderRegistry([
+                $app->make(FakeAccessProvider::class),
+            ]),
+        );
     }
 
     public function boot(): void
