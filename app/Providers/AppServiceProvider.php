c3c33b79bdca8ff96c7ff37facf7ffe8707c3a26
Rayan TSOLEFACK <68066376+rayanfalcao8@users.noreply.github.com>
2026-07-16 23:56:56 -0400

    refactor(payment): require confirmation before access

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,9 @@ namespace App\Providers;
 
 use App\Domain\Access\Contracts\NetworkAccessProvider;
 use App\Domain\Access\Services\NetworkProviderRegistry;
+use App\Domain\Payment\Services\PaymentProviderRegistry;
 use App\Infrastructure\Network\FakeAccessProvider;
+use App\Infrastructure\Payment\FakePaymentProvider;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,11 +15,18 @@ class AppServiceProvider extends ServiceProvider
     {
         $this->app->singleton(FakeAccessProvider::class);
         $this->app->bind(NetworkAccessProvider::class, FakeAccessProvider::class);
-
         $this->app->singleton(
             NetworkProviderRegistry::class,
             fn ($app) => new NetworkProviderRegistry([
                 $app->make(FakeAccessProvider::class),
+            ]),
+        );
+
+        $this->app->singleton(FakePaymentProvider::class);
+        $this->app->singleton(
+            PaymentProviderRegistry::class,
+            fn ($app) => new PaymentProviderRegistry([
+                $app->make(FakePaymentProvider::class),
             ]),
         );
     }
